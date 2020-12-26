132a6eb0f06634e7faf0ac89061a377b6b577a6d
Nuno <9170511@esmad.ipp.pt>
2020-12-26 21:41:53 +0000

    Associar unique index a email e imei

--- models/user.model.js
+++ models/user.model.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     givenName: { type: String, required: true },
     familyName: { type: String, required: true },
-    email: { type: String, required: true, lowercase: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
     password: { type: String, required: true },
     pin: { type: Number },
     phoneList: [{
         type: {
             model: { type: String },
-            imei: { type: String }
+            imei: { type: String, unique: true }
         }
     }],
     floors: [{ type: mongoose.Schema.Types.ObjectId, ref: "floor" }],
